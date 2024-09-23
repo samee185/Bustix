00713c365cf234b3d5ec0e3d08d9ec5cd690b74e
samee185 <samueliy185@gmail.com>
2024-09-23 16:41:03 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthProvider from "./contexts/AuthContext";
 import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn';
+import Home from './pages/Home';
+
+document.title = "Bustix"
 const App = () => {
+
   return (
     <>
       <Router>
         <AuthProvider>
               <Routes>
                 <Route path="/" element={<SignUp />} />
-                {/* <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/users" element={<User />} />
-                <Route path="/bus" element={<Bus />} />
+                <Route path="/signup" element={<Home />} />
+                <Route path="/login" element={<LogIn />} />
+                {/* <Route path="/bus" element={<Bus />} />
                 <Route path="/profile" element={<Profile />} /> */}
               </Routes>
               <ToastContainer />
